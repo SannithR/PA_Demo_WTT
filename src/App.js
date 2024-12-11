3d00c3088167f6fa59ac36a80df89b4be38b4c5a
Sannith <sannithpeddolla143@gmail.com>
2024-12-11 16:41:59 +0530

    implemented some variables to use tokenization

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styleguide.css';
 import SinglePlayerBoard from "./components/SinglePlayerBoard/SinglePlayerBoard";
 
 import { Routes, Route } from "react-router-dom";
+import './styleguide1.css';
 
 
 import Sample from "./components/Sample";
